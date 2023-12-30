992da2f1f4dc44539cd3c709c723e78e4b21718f
captjay98 <captjay98@gmail.com>
2023-12-30 02:19:42 +0100

    Routes

---
+++ routes/web.php
@@ -0,0 +1,106 @@
+<?php
+
+use App\Http\Controllers\BookingController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TicketController;
+use App\Http\Controllers\TripController;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
+Route::get('/dashboard', function () {
+    return Inertia::render('profile.edit');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+Route::get('/trips', [TripController::class, 'index'])->name('trips.all');
+Route::get('/trips/{trip_id}', [TripController::class, 'show'])->name('trips.one');
+Route::post('/trips', [TripController::class, 'SearchTrips'])->name('trips.search');
+Route::get('/tickettype', [TicketTypeController::class, 'allTicketTypes'])->name('ticketypes.all');
+Route::post('/tickettype', [TicketTypeController::class, 'selectTicketTypes'])->name('ticketypes.select');
+Route::get('trips/{trip_id}/coaches/{tickettype}', [BookingController::class, 'getCoaches'])->name('coaches.all');
+Route::get('trips/{trip_id}/coaches/{coach_id}/seats', [BookingController::class, 'getAvailableSeats'])->name('seats.all');
+Route::middleware(['auth'])->group(function () {
+    Route::post('/seats/reserve', [BookingController::class, 'reserveSeat'])->name('seats.reserve');
+    Route::get('/pay', [App\Http\Controllers\PaymentController::class, 'redirectToGateway'])->name('pay');
+    Route::get('/payment/callback', [App\Http\Controllers\PaymentController::class, 'handleGatewayCallback'])->name('payment');
+    Route::get('/success', [App\Http\Controllers\PaymentController::class, 'success'])->name('succesful');
+    Route::post('paystack/webhook', [\App\Http\Controllers\WebhookController::class, 'handleWebhook'])->name('webhook');
+    Route::get('/ticket/create', [TicketController::class, 'createTicket'])->name('ticket.create');
+    Route::get('ticket/show',[TicketController::class, 'showTicket'])->name('ticket.show');
+
+});
+
+// Route::middleware('throttle:2:1');
+// Route::middleware(['auth', 'is_admin:"true"'])->group(function () {
+
+Route::middleware(['auth', 'checkStaff'])->group(function () {
+    Route::name('admin.')->prefix('admin')->group(function () {
+        Route::get('/dashboard', [AdminController::class, 'index'])->name('dashboard');
+
+        Route::get('/users', [AdminController::class, 'allUsers'])->name('users.all');
+        Route::get('/users/{user_id}', [AdminController::class, 'oneUser'])->name('users.one');
+        Route::put('/users/{user_id}', [AdminController::class, 'deactivateUser'])->name('users.deactivate')->middleware('checkAdmin');
+
+        Route::get('/travellers', [AdminController::class, 'allTravellers'])->name('Travellers.all');
+        Route::get('/travellers/{user_id}', [AdminController::class, 'oneTraveller'])->name('traveller.one');
+
+        Route::get('/trips', [AdminController::class, 'allTrips'])->name('trips.all');
+        Route::get('/trips/create', [AdminController::class, 'showCreateTrip'])->name('trips.showcreate')->middleware('checkAdmin');
+        Route::post('/trips', [AdminController::class, 'createTrip'])->name('trips.create')->middleware('checkAdmin');
+        Route::patch('/trips', [AdminController::class, 'updateTrip'])->name('trips.update')->middleware('checkAdmin');
+        Route::get('/trips/{trip_id}', [AdminController::class, 'oneTrip'])->name('trips.one');
+
+        Route::get('/tickets', [AdminController::class, 'allTickets'])->name('tickets.all');
+        Route::post('/tickets/create', [AdminController::class, 'createTicket'])->name('tickets.create')->middleware('checkAdmin');
+        Route::put('/tickets/update', [AdminController::class, 'updateTicket'])->name('tickets.update')->middleware('checkAdmin');
+        Route::get('/tickets/{ticket_id}', [AdminController::class, 'oneTicket'])->name('tickets.one');
+
+        Route::get('/tickettypes', [AdminController::class, 'allTicketTypes'])->name('tickettypes.all');
+        Route::get('/tickettype/create', [AdminController::class, 'showCreateTicketType'])->name('tickettypes.showcreate')->middleware('checkAdmin');
+        Route::post('/tickettype/create', [AdminController::class, 'createTicketType'])->name('tickettypes.create')->middleware('checkAdmin');
+        Route::put('/tickettype/update', [AdminController::class, 'updateTicketType'])->name('tickettypes.update')->middleware('checkAdmin');
+        Route::get('/tickettype/{tickettype}', [AdminController::class, 'oneTicketType'])->name('tickettypes.one');
+
+        Route::get('/coaches', [AdminController::class, 'allCoaches'])->name('coaches.all');
+        Route::get('/coaches/create', [AdminController::class, 'showCreateCoach'])->name('coaches.showcreate')->middleware('checkAdmin');
+        Route::post('/coaches/create', [AdminController::class, 'createCoach'])->name('coaches.create')->middleware('checkAdmin');
+        Route::put('/coaches/update', [AdminController::class, 'updateCoach'])->name('coaches.update')->middleware('checkAdmin');
+        Route::get('/coaches/{coaches}', [AdminController::class, 'oneCoach'])->name('coaches.one');
+
+        Route::get('/seats', [AdminController::class, 'allSeats'])->name('seats.all');
+        Route::get('/seats/{seat_id}', [AdminController::class, 'oneSeat'])->name('seats.one');
+
+        Route::get('/bookings', [AdminController::class, 'allBookings'])->name('bookings.all');
+        Route::get('/bookings/{booking_id}', [AdminController::class, ' oneBooking'])->name('bookings.one');
+    });
+});
+
+require __DIR__.'/auth.php';
