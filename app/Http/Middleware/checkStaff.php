961acae615d681c4d619eb2e1f2239237ad76cd3
captjay98 <captjay98@gmail.com>
2023-12-30 02:15:31 +0100

    Middlewares

---
+++ app/Http/Middleware/checkStaff.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class checkStaff
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = $request->user();
+        if($user->is_staff  === true && $user->account_type === 'staff') {
+
+            return $next($request);
+        } else {
+            return  abort(403, 'unauthorized');
+        }
+    }
+}
---
+++ app/Http/Middleware/checkSuperuser.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class checkSuperuser
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = $request->user();
+        if($user->is_superuser === true && $user->accountype === "superuser") {
+            return $next($request);
+        } else {
+            return abort(403, 'Unauthorized');
+        }
+    }
+}
