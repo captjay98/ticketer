c94a2bf11c11b9c4fa4d75f439b7d4b0809ef237
captjay98 <captjay98@gmail.com>
2023-12-30 02:06:31 +0100

    Event and Listener for Populating Seats after Trip Creation

---
+++ app/Listeners/TripCreatedListener.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\TripCreated;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use App\Models\Seat;
+use App\Models\TicketType;
+
+class TripCreatedListener
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(TripCreated $event): void
+    {
+        $trip = $event->trip;
+
+        $coaches = $trip->coaches;
+
+        $tickettypes = [
+            'KD_ABJ' => ['Economy' => 4000,'Business' => 7000,],
+            'ABJ_KD' => ['Economy' => 4000,'Business' => 7000,],
+            'LAG_IBD' => ['Economy' => 5000,'Business' => 9000,],
+            'IBD_LAG' => ['Economy' => 5000,'Business' => 9000,],
+            'WAR_IBD' => ['Economy' => 5000,'Business' => 10000,],
+            'IBD_WAR' => ['Economy' => 5000,'Business' => 10000,],
+            'KN_KD' => ['Economy' => 4500,'Business' => 7500,],
+            'KD_KN' => ['Economy' => 4500,'Business' => 7500,],
+            'KN_ABJ' => ['Economy' => 5000,'Business' => 9000,],
+            'ABJ_KN' => ['Economy' => 5000,'Business' => 9000,],
+            'WAR_LAG' => ['Economy' => 6000,'Business' => 9000,],
+            'LAG_WAR' => ['Economy' => 6000,'Business' => 9000,],
+        ];
+
+
+        foreach($coaches as $coach) {
+            $totalseats = ($coach->coach_class === 'Economy') ? 54 : 28;
+            for ($i = 1; $i <= $totalseats ; $i++) {
+                Seat::create([
+                    'seat_number' => $i,
+                    'trip_id' => $trip->id,
+                    'coach_id' => $coach->id,
+                    'status' => 'available'
+                ]);
+            }
+        }
+
+        foreach ($tickettypes[$trip->title] as $class => $price) {
+            // dd($tickettypes[$trip->title], $class, $price);
+            TicketType::create([
+                'price' => $price,
+                'seat_class' => $class,
+                'trip_id' => $trip->id,
+            ]);
+        }
+
+
+    }
+}
