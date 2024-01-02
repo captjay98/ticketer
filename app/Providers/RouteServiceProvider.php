b53e96694993dc951d332c02fd2155dd0c6d1a19
captjay98 <captjay98@gmail.com>
2024-01-02 17:24:08 +0100

    Admin redirecion fix

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,7 +18,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/profile';
-    public const ADMIN = '/admin';
+    public const ADMINHOME = '/admin/dashboard';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
