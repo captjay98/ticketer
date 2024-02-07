3fac6e9daa72dc7d3238a8f6756e57c56989282c
captjay98 <captjay98@gmail.com>
2024-02-07 17:33:13 +0100

    Oncascade

--- database/migrations/2023_09_12_193504_seats.php
+++ database/migrations/2023_09_12_193504_seats.php
@@ -4,23 +4,27 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class () extends Migration {
+return new class() extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('seats', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('seat_number');
-            $table->string('seat_position')->nullable();
-            $table->integer('trip_id');
-            $table->integer('coach_id');
-            $table->string('status')->default('available')->enum('available', 'reserved', 'booked');
-            $table->foreign('trip_id')->references('id')->on('trips');
-            $table->foreign('coach_id')->references('id')->on('coaches');
-            $table->timestamps();
-        });
+        Schema::create(
+            'seats',
+            function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->string('seat_number');
+                $table->string('seat_position')->nullable();
+                $table->integer('trip_id');
+                $table->integer('coach_id');
+                $table->string('status')->default('available')->enum('available', 'reserved', 'booked');
+                $table->foreign('trip_id')->references('id')->on('trips')->onDelete('cascade');
+                $table->foreign('coach_id')->references('id')->on('coaches');
+                $table->timestamps();
+            }
+        );
         //
     }
 
--- database/migrations/2023_09_16_193735_coach_trips.php
+++ database/migrations/2023_09_16_193735_coach_trips.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class () extends Migration {
+return new class() extends Migration
+{
     /**
      * Run the migrations.
      */
@@ -13,8 +14,8 @@ return new class () extends Migration {
         Schema::create('coach_trips', function (Blueprint $table) {
             $table->integer('trip_id');
             $table->integer('coach_id');
-            $table->foreign('trip_id')->references('id')->on('trips');
-            $table->foreign('coach_id')->references('id')->on('coaches');
+            $table->foreign('trip_id')->references('id')->on('trips')->onDelete('cascade');
+            $table->foreign('coach_id')->references('id')->on('coaches')->onDelete('cascade');
             $table->timestamps();
         });
     }
