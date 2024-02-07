27bf9caf1eb6f7dc20700b973710bb4e75b5f97c
captjay98 <captjay98@gmail.com>
2024-02-07 22:01:12 +0100

    Added pagination

---
+++ resources/js/Components/Pagination.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { Link } from "@inertiajs/vue3";
+const { links } = defineProps({
+    links: Array,
+});
+</script>
+<template>
+    <div v-if="links.length > 3">
+        <div class="flex flex-wrap -mb-1">
+            <template v-for="(link, key) in links" :key="key">
+                <div
+                    v-if="link.url === null"
+                    class="py-3 px-4 mr-1 mb-1 text-sm leading-4 text-gray-400 rounded border"
+                    v-html="link.label"
+                />
+                <Link
+                    v-else
+                    class="py-3 px-4 mr-1 mb-1 text-sm leading-4 rounded border hover:bg-white focus:text-indigo-500 focus:border-indigo-500"
+                    :class="{ 'bg-white': link.active }"
+                    :href="link.url"
+                    v-html="link.label"
+                />
+            </template>
+        </div>
+    </div>
+</template>
--- resources/js/Pages/Admin/Seats.vue
+++ resources/js/Pages/Admin/Seats.vue
@@ -1,63 +1,78 @@
 <script setup>
 import AdminLayout from "@/Layouts/AdminLayout.vue";
+import Pagination from "@/Components/Pagination.vue";
 import { Link } from "@inertiajs/vue3";
 
-const { seats } = defineProps({
-    seats: Object,
+const { items } = defineProps({
+    items: Object,
 });
+
+const seats = items.data;
 
 console.log(seats);
 </script>
 <template>
     <AdminLayout>
-        <div class="w-full rounded-md px-4 py-4 m-auto bg-yellow-50/10">
-            <div class="mb-2 mt-1 m-auto w-full px-2 py-2 rounded-md">
-                <h1 class="text-2xl tracking-wide text-center font-semibold py-4">All seats</h1>
+        <div class="py-4 px-4 m-auto w-full rounded-md bg-yellow-50/10">
+            <div class="py-2 px-2 m-auto mt-1 mb-2 w-full rounded-md">
+                <h1 class="py-4 text-2xl font-semibold tracking-wide text-center">
+                    All seats
+                </h1>
                 <!-- <Link :href="route('admin.seats.showcreate')"><p>Create seat</p></Link> -->
             </div>
             <div v-show="seats.length < 1">
-                <h1 class="text-xl font-semibold text-center pb-4">No Seats Created</h1>
+                <h1 class="pb-4 text-xl font-semibold text-center">
+                    No Seats Created
+                </h1>
             </div>
-            <div class="py-1 w-full min-h-screen m-auto rounded-lg overflow-auto">
-                <table class="min-w-full divide-y divide-green-50/300 divide-solid rounded-md">
-                    <thead class="bg-gray-200/40 text-left text-[13px] rounded-lg">
+            <div class="w-full">
+                <div v-for="item in items.data" :key="item.id">
+                    {{ item.name }}
+                </div>
+                <Pagination :links="items.links" />
+            </div>
+            <div class="overflow-auto py-1 m-auto w-full min-h-screen rounded-lg">
+                <table class="min-w-full rounded-md divide-y divide-solid divide-green-50/300">
+                    <thead class="text-left rounded-lg bg-gray-200/40 text-[13px]">
                         <tr class="text-left border-inherit">
-                            <th class="px-2 py-2 text-center text-blue-800">ID</th>
-                            <th class="px-2 py-2">Seat Number</th>
-                            <th class="px-2 py-2">seat Status</th>
-                            <th class="px-2 py-2">Trip</th>
-                            <th class="px-2 py-2">Coach</th>
-                            <th class="px-2 py-2">Coach Class</th>
-                            <th class="px-2 py-2">Date Created</th>
+                            <th class="py-2 px-2 text-center text-blue-800">
+                                ID
+                            </th>
+                            <th class="py-2 px-2">Seat Number</th>
+                            <th class="py-2 px-2">seat Status</th>
+                            <th class="py-2 px-2">Trip</th>
+                            <th class="py-2 px-2">Coach</th>
+                            <th class="py-2 px-2">Coach Class</th>
+                            <th class="py-2 px-2">Date Created</th>
                         </tr>
                     </thead>
-                    <tbody class="bg-green-50/30 divide-solid rounded-lg">
-                        <tr v-for="seat in seats" class="text-[12px] odd:bg-gray-200/40 rounded-md">
-                            <td class="px-2 py-2 font-bold text-blue-800 text-center">
+                    <tbody class="rounded-lg divide-solid bg-green-50/30">
+                        <tr v-for="seat in seats" class="rounded-md text-[12px] odd:bg-gray-200/40">
+                            <td class="py-2 px-2 font-bold text-center text-blue-800">
                                 {{ seat.id }}
                             </td>
 
-                            <td class="px-8 py-2 min-w-[100px] max-w-[180px] leading-5">
+                            <td class="py-2 px-8 leading-5 min-w-[100px] max-w-[180px]">
                                 <Link class="" :href="route('admin.seats.one', seat.id)">
-                                    {{ seat.seat_number }}
+                                {{ seat.seat_number }}
                                 </Link>
                             </td>
 
-                            <td class="px-2 py-2 min-w-[100px] max-w-[180px] leading-5">
+                            <td class="py-2 px-2 leading-5 min-w-[100px] max-w-[180px]">
                                 {{ seat.status }}
                             </td>
 
-                            <td class="px-2 py-2 min-w-[100px] max-w-[180px] leading-5">
+                            <td class="py-2 px-2 leading-5 min-w-[100px] max-w-[180px]">
                                 {{ seat.trip.title }}
                             </td>
-                            <td class="px-2 py-2 min-w-[100px] max-w-[180px] leading-5">
+                            <td class="py-2 px-2 leading-5 min-w-[100px] max-w-[180px]">
                                 {{ seat.coach.coach_name }}
                             </td>
-                            <td class="px-2 py-2 min-w-[100px] max-w-[180px] leading-5">
+                            <td class="py-2 px-2 leading-5 min-w-[100px] max-w-[180px]">
                                 {{ seat.coach.coach_class }}
                             </td>
 
-                            <td class="px-2 py-2 min-w-[100px] max-w-[180px] leading-5">
+                            <td class="py-2 px-2 leading-5 min-w-[100px] max-w-[180px]">
                                 {{ seat.created_at }}
                             </td>
                         </tr>
